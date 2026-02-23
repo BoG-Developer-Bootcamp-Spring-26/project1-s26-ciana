83beae8cee2fa60fac52ee21fa4916c73e3c9b32
Ciana <Cianalee2025@Gmail.com>
2026-02-23 15:43:08 -0500

    fixed identifier/info sections and started styles

--- script.js
+++ script.js
@@ -1,36 +1,90 @@
+let id= 100;
+let data = null;
+let panel = true;
+const statType = document.getElementById("stat-type");
+const statInfo = document.getElementById("stat-info");
 const incButton = document.getElementById("inc-button");
 const decButton = document.getElementById("dec-button");
-let id= 100;
+
+const infoButton = document.getElementById("info-button");
+const movesButton = document.getElementById("moves-button");
 
 fetchPokemon(id);
 
 incButton.addEventListener("click", async (event) => {
     fetchPokemon(++id);
+    updateInfo();
+    updateMoves();
+
+   
 });
 
 decButton.addEventListener("click", async (event) => {
     if(id > 0) {
         fetchPokemon(--id);
+        updateInfo();
+        updateMoves();
     }
-
 });
 
 async function fetchPokemon(id){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    const data = await response.json();
+    data = await response.json();
 
-    const pokeImage = document.getElementById("pokemon-img");
-    pokeImage.src = data.sprites.front_shiny;
+    document.getElementById("pokemon-img").src = data.sprites.front_shiny;
 
-    const nameText= document.getElementById("pokemon-name");
-    nameText.textContent = data.name;
+    document.getElementById("pokemon-name").textContent = data.name;
 
-    const pokeType = document.getElementById("types");
-    pokeType.textContent = data.types.map( (type) => {
-        return type.type.name;
+    let typesArea = document.getElementById("types")
+    typesArea.textContent = "";
+
+    data.types.forEach((type) => {
+            const element = document.createElement("p");
+            element.textContent = type.type.name;
+            typesArea.appendChild(element);
     })
+}
 
-    const infoButton = document.getElementById("info-button");
-    infoButton.addEventListener("click")
-    const movesButton = document.getElementById("moves-button");
+
+infoButton.addEventListener("click", () => {
+    panel = true;
+    updateInfo();
+    
+});
+
+
+movesButton.addEventListener("click", () => {  
+    panel = false; 
+    updateMoves();   
+    
+});
+
+
+function updateMoves(){
+    if(!panel) {
+        statType.textContent = "Moves";
+            
+            statInfo.innerHTML="";
+            statInfo.innerHTML = `
+                <p>${data.moves[0].move.name}</p>
+            `;
+    
+    }
+}
+
+function updateInfo(){
+
+    if(panel) {
+        statType.textContent = "Info";
+
+            const stats = data.stats.map( s => (
+                `<p>${s.stat.name}: ${s.base_stat}</p>`
+            )).join("");
+
+            statInfo.innerHTML = `
+                <p>height: ${data.height / 10} m</p>
+                <p>weight: ${data.weight / 10} kg</p>
+                ${stats}
+            `;
+    }
 }
